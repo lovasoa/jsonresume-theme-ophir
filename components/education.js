38b80a7f3974d089ac70cdff8345556d4cc76bac
lovasoa <pere.jobs@gmail.com>
2023-05-26 19:37:30 +0200

    education labels

--- components/education.js
+++ components/education.js
@@ -4,29 +4,43 @@ import Duration from './duration.js'
 import Link from './link.js'
 
 export default function Education(education = []) {
-  return education.length > 0 && html`
-    <section id="education">
-      <h3>Education</h3>
-      <div class="stack">
-        ${education.map(({ area, courses = [], institution, startDate, endDate, studyType, url }) => html`
-          <article>
-            <header>
-              <h4>${Link(url, institution)}</h4>
-              <div class="meta">
-                ${area && html`<strong>${area}</strong>`}
-                <div>${Duration(startDate, endDate)}</div>
-              </div>
-            </header>
-            ${studyType && markdown(studyType)}
-            ${courses.length > 0 && html`
-              <h5>Courses</h5>
-              <ul>
-                ${courses.map(course => html`<li>${markdown(course)}</li>`)}
-              </ul>
-            `}
-          </article>
-        `)}
-      </div>
-    </section>
-  `
+  return (
+    education.length > 0 &&
+    html`
+      <section id="education">
+        <h3>Education</h3>
+        <div class="stack">
+          ${education.map(
+            ({
+              area,
+              courses = [],
+              institution,
+              startDate,
+              endDate,
+              studyType,
+              url,
+            }) => html`
+              <article>
+                <header>
+                  <h4>${Link(url, institution)}</h4>
+                  <div class="meta">
+                    ${area && html`<strong>${area}</strong>`}
+                    <div>${Duration(startDate, endDate)}</div>
+                  </div>
+                </header>
+                ${studyType && markdown(studyType)}
+                ${courses.length > 0 &&
+                html`
+                  <h5>Courses</h5>
+                  <ul class="tag-list">
+                    ${courses.map(course => html`<li>${markdown(course)}</li>`)}
+                  </ul>
+                `}
+              </article>
+            `,
+          )}
+        </div>
+      </section>
+    `
+  )
 }
